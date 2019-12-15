f863ca3db963b6ef0b816c121049916c2633f63b
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2019-12-15 18:44:11 +0100

    method name changed

--- src/main/java/HashCodes/HashCode.java
+++ src/main/java/HashCodes/HashCode.java
@@ -10,21 +10,21 @@ public class HashCode {
     public static void main(String args[]) {
         final String text = "xd";
 
-        runner(() -> HashCodeUtils.getHashMD5(text), text, "MD-5");
-        runner(() -> HashCodeUtils.getHashSHA1(text), text, "SHA-1");
+        printInfo(() -> HashCodeUtils.getHashMD5(text), text, "MD-5");
+        printInfo(() -> HashCodeUtils.getHashSHA1(text), text, "SHA-1");
 
-        runner(() -> HashCodeUtils.getHashSHA224(text), text, "SHA2-224");
-        runner(() -> HashCodeUtils.getHashSHA256(text), text, "SHA2-256");
-        runner(() -> HashCodeUtils.getHashSHA384(text), text, "SHA2-384");
-        runner(() -> HashCodeUtils.getHashSHA512(text), text, "SHA2-512");
+        printInfo(() -> HashCodeUtils.getHashSHA224(text), text, "SHA2-224");
+        printInfo(() -> HashCodeUtils.getHashSHA256(text), text, "SHA2-256");
+        printInfo(() -> HashCodeUtils.getHashSHA384(text), text, "SHA2-384");
+        printInfo(() -> HashCodeUtils.getHashSHA512(text), text, "SHA2-512");
 
-        runner(() -> HashCodeUtils.getHashSHA3_224(text), text, "SHA3-224");
-        runner(() -> HashCodeUtils.getHashSHA3_256(text), text, "SHA3-256");
-        runner(() -> HashCodeUtils.getHashSHA3_384(text), text, "SHA3-384");
-        runner(() -> HashCodeUtils.getHashSHA3_512(text), text, "SHA3-384");
+        printInfo(() -> HashCodeUtils.getHashSHA3_224(text), text, "SHA3-224");
+        printInfo(() -> HashCodeUtils.getHashSHA3_256(text), text, "SHA3-256");
+        printInfo(() -> HashCodeUtils.getHashSHA3_384(text), text, "SHA3-384");
+        printInfo(() -> HashCodeUtils.getHashSHA3_512(text), text, "SHA3-384");
     }
 
-    private static void runner(Supplier runnable, String text, String method) {
+    private static void printInfo(Supplier runnable, String text, String method) {
         final double repeats = 10000000;
         String result = (String) runnable.get();
         System.out.println("Input:" + text);
